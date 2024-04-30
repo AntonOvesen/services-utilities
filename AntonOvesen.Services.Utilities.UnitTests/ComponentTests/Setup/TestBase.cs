a58d77184e9d6489b92b3c523c7dfad536e54b1f
Anton Ovesen <anton@ovesen-mail.dk>
2024-04-30 19:57:04 +0200

    wip

---
+++ AntonOvesen.Services.Utilities.UnitTests/ComponentTests/Setup/TestBase.cs
@@ -0,0 +1,6 @@
+﻿namespace AntonOvesen.Services.Utilities.Tests.ComponentTests.Setup
+{
+    public class TestBase : Bunit.TestContext
+    {
+    }
+}
---
+++ AntonOvesen.Services.Utilities.UnitTests/E2ETests/Pages/ConverterPageTest.cs
@@ -0,0 +1,11 @@
+//namespace AntonOvesen.Services.Utilities.Tests.E2ETests.Pages;
+
+//[Parallelizable(ParallelScope.Self)]
+//[TestFixture]
+//internal class ConverterPageTest
+//{
+//    [Test]
+//    public async Task Convert_ModeIsAuto_DetectsUUIDAndConvertsToBase64()
+//    {
+//    }
+//}
